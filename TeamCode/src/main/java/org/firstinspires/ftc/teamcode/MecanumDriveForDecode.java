99e2624d4b0d11ea79f1ac8f7f8015d74e71cd12
Ben Wang <ben.wang@ncsnanaimo.com>
2025-12-06 10:51:30 -0800

    changed autonomous code to fit new robot model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveForDecode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveForDecode.java
@@ -2,17 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Mecanum drive for Decode 2025-2026", group = "TeleOp")
 public class MecanumDriveForDecode extends LinearOpMode {
-    public DcMotorEx frontLeft, frontRight, backLeft, backRight, arm1, arm2, ejector1, ejector2 = null;
-    public Servo elevator = null;
-
-    private double armPosition = 0;
+    public DcMotorEx frontLeft, frontRight, backLeft, backRight, ejector1, ejector2 = null;
+    public CRServo elevator, intake = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,18 +19,15 @@ public class MecanumDriveForDecode extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
         backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
         backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-        arm1 = hardwareMap.get(DcMotorEx.class, "arm1");
-        arm2 = hardwareMap.get(DcMotorEx.class, "arm2");
         ejector1 = hardwareMap.get(DcMotorEx.class, "ejector1");
         ejector2 = hardwareMap.get(DcMotorEx.class, "ejector2");
-        elevator = hardwareMap.get(Servo.class, "elevator");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        elevator = hardwareMap.get(CRServo.class, "elevator");
         // Reverse the right side motors for proper direction
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        arm1.setDirection(DcMotorSimple.Direction.FORWARD);
-        arm2.setDirection(DcMotorSimple.Direction.FORWARD);
         ejector1.setDirection(DcMotorSimple.Direction.FORWARD);
         ejector2.setDirection(DcMotorSimple.Direction.FORWARD);
 
@@ -44,19 +39,6 @@ public class MecanumDriveForDecode extends LinearOpMode {
         ejector1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         ejector2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         waitForStart();
-
-        arm1.setPower(1);
-        arm2.setPower(1);
-
-        arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        arm1.setTargetPosition(0);
-        arm2.setTargetPosition(0);
-
-        arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        arm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
 
         while (opModeIsActive()) {
             // Get joystick values
@@ -95,19 +77,9 @@ public class MecanumDriveForDecode extends LinearOpMode {
                 backRight.setPower(backRightPower);
             }
 
-            //arm control
-            double armPower = gamepad2.left_stick_y;
-            double ejectorPower = 0.5 * gamepad2.right_stick_y;
+            elevator.setPower(gamepad2.left_stick_y);
+            intake.setPower(1);
 
-            armPosition += armPower;
-
-            ejector1.setPower(ejectorPower);
-            ejector2.setPower(ejectorPower);
-
-            arm1.setTargetPosition(Math.min((int) Math.round(armPosition), 1000));
-            arm2.setTargetPosition(Math.min((int) Math.round(armPosition), 1000));
-
-            telemetry.addData("arm position", armPosition);
             telemetry.addData("forward/backward power", fb);
             telemetry.addData("strafe power", strafe);
             telemetry.addData("turn power", turn);
