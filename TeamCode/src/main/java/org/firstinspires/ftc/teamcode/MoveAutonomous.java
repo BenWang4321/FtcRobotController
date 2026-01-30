feaa9d1ab051d6652feaedf81ef9b2c52c637025
supraGitAccount <uriahpoh@hotmail.com>
2026-01-29 16:05:46 -0800

    added a bunch of stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveAutonomous.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name = "move off line", group = "autonomous")
 public class MoveAutonomous extends LinearOpMode {
-    private DcMotorEx frontLeft, frontRight, backLeft, backRight = null;
+    public DcMotorEx frontLeft, frontRight, backLeft, backRight = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,26 +16,28 @@ public class MoveAutonomous extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
         backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
         backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-        DcMotorEx[] motorGroup = new DcMotorEx[]{frontLeft, frontRight, backLeft, backRight};
 
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        DcMotorEx[] motorGroup = new DcMotorEx[]{frontLeft, frontRight, backLeft, backRight};
+
+        waitForStart();
+
         for (DcMotorEx motor : motorGroup) {
-            motor.setTargetPosition(0);
+            motor.setTargetPosition(40000);
             motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             motor.setPower(1);
         }
 
-        waitForStart();
-
         while (opModeIsActive()) {
             for (DcMotorEx motor : motorGroup) {
-                motor.setTargetPosition(4000);
+                telemetry.addData(motor.getDeviceName() + " current position", motor.getCurrentPosition());
+                telemetry.addData(motor.getDeviceName() + " velocity", motor.getVelocity());
             }
+            telemetry.update();
         }
-
     }
 }
